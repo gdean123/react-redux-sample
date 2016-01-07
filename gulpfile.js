b84275e0567e0bab5702d80c7fe0193b5f5c5194
Adam Berkovec and Christa Hartsock <chartsock@pivotal.io>
2016-01-06 17:24:22 -0800

    set up sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var jasmineBrowser = require('gulp-jasmine-browser');
 var webpack = require('webpack-stream');
+var sass = require('gulp-sass');
+var path = require('path');
+var concat = require('gulp-concat');
 
 var JasminePlugin = require('gulp-jasmine-browser/webpack/jasmine-plugin');
 var plugin = new JasminePlugin();
@@ -29,4 +32,16 @@ gulp.task('jasmine-phantom', function () {
         .pipe(webpack(webpackConfiguration))
         .pipe(jasmineBrowser.specRunner({console: true}))
         .pipe(jasmineBrowser.headless());
+});
+
+gulp.task('sass', function () {
+    return gulp.src(path.join(__dirname, 'src/stylesheets/**/*.sass'))
+        .pipe(sass())
+        .pipe(gulp.dest(path.join(__dirname, 'tmp')))
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(path.join(__dirname, 'assets')));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch(path.join(__dirname, 'src/stylesheets/**/*.sass'), ['sass']);
 });
