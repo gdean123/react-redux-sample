cb0bb9584768e5eb3ec99da18ccb9460d6d1c591
Adam Berkovec and George Dean <pair+aberkovec+gdean@pivotal.io>
2016-01-08 17:35:06 -0800

    npm run test-headless runs jasmine tests headlessly using PhantomJS

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ var webpack = require('webpack-stream');
 var sass = require('gulp-sass');
 var path = require('path');
 var concat = require('gulp-concat');
-
+var objectAssign = require('object-assign');
 var JasminePlugin = require('gulp-jasmine-browser/webpack/jasmine-plugin');
+
 var plugin = new JasminePlugin();
 
 var webpackConfiguration = {
-    watch: true,
     output: {filename: 'spec.js'},
     plugins: [plugin],
     module: {
@@ -21,17 +21,20 @@ var webpackConfiguration = {
 
 gulp.task('jasmine', function () {
     return gulp.src(['spec/**/*Spec.js'])
-        .pipe(webpack(webpackConfiguration))
+        .pipe(webpack(objectAssign({}, webpackConfiguration, {watch: true})))
         .pipe(jasmineBrowser.specRunner())
         .pipe(jasmineBrowser.server({whenReady: plugin.whenReady}));
 });
 
-gulp.task('jasmine-phantom', function () {
-    console.log(jasmineBrowser.specRunner({console: true}));
-    return gulp.src(['spec/**/*Spec.js'])
+gulp.task('jasmine-headless', function () {
+    return gulp.src([
+        './node_modules/babel-polyfill/dist/polyfill.js',
+        './node_modules/phantomjs-polyfill/bind-polyfill.js',
+        'spec/**/*Spec.js'
+    ])
         .pipe(webpack(webpackConfiguration))
         .pipe(jasmineBrowser.specRunner({console: true}))
-        .pipe(jasmineBrowser.headless());
+        .pipe(jasmineBrowser.headless({driver: 'phantomjs'}));
 });
 
 gulp.task('sass', function () {
